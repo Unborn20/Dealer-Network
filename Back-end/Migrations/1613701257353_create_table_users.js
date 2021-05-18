45dc271e0dc6aae2c2ae27430b6b7fad1132d520
Unborn20 <rianosergio376@gmail.com>
2021-05-17 23:29:21 -0500

    Fixed migrations and enviroment variables

--- Back-end/Migrations/1613701257353_create_table_users.js
+++ Back-end/Migrations/1613701257353_create_table_users.js
@@ -4,7 +4,7 @@ module.exports = {
   				"	`userName` varchar(45) NOT NULL,"+
 				  "	`roleId` int(11) NOT NULL DEFAULT '2',"+
 				  " `email` varchar(50) NOT NULL,"+
-				  " `userPassword` varchar(45) NOT NULL,"+
+				  " `userPassword` varchar(255) NOT NULL,"+
 				  " `registerDate` TIMESTAMP,"+
 				  " PRIMARY KEY (`idusers`),"+ 
 				  " CONSTRAINT `fk_roleId` FOREIGN KEY (`roleId`) REFERENCES `roles` (`idroles`)"+
--- Back-end/migration.js
+++ Back-end/migration.js
@@ -1,13 +1,13 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const migration = require('mysql-migrations');
-require('dotenv').config();
+require('dotenv').config({ path: '.env' })
 
 const connection = mysql.createPool({
   connectionLimit : 2,
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME
 });
 
 migration.init(connection, __dirname + '/Migrations', () => {
